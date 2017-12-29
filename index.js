f85d81c4c6f2cc38ee61cb8498836d937462d11b
Ilche <ilcheivanovski@gmail.com>
2017-12-29 13:21:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World 1!");
+    response.end("Hello World 2!");
 });
 
 var port = process.env.PORT || 1337;
